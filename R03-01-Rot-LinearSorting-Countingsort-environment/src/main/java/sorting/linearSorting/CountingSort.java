177f1b1cd1dbd44e9f65cd28ad078e862a076ee3
Gabriel Fernandes <gabrielwebr@gmail.com>
2019-04-04 22:44:27 -0300

    Update R03

--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -12,8 +12,75 @@ public class CountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		
+		if(leftIndex > rightIndex || array == null || array.length == 0 || array.length < rightIndex || leftIndex > array.length) {
+			return;
+		}
+		
+		int k = biggestElement(array, leftIndex, rightIndex);
+		
+		Integer[] c = new Integer[k + 1];
+		
+		fillArray(c);
+		
+		for (int i = 0; i < rightIndex + 1; i++) {
+			c[array[i]]++;
+		}
+		
+		for (int i = 1; i < c.length; i++) {
+			c[i] = c[i] + c[i - 1];
+		}
+		
+		Integer[] b = new Integer[array.length];
+		
+		for (int i = rightIndex; i >= 0; i --) {
+			b[c[array[i]] - 1]  = array[i];
+			c[array[i]]--;
+		}
+		
+		copy(array, b);
 	}
-
+	
+	/**
+	 * It fills in all spaces of the vector with 0
+	 * 
+	 * @param array Vector to be filled
+	 */
+	private void fillArray(Integer[] array) {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = 0;
+		}
+	}
+	
+	/**
+	 * It returns the biggest element of the vector
+	 * 
+	 * @param array Array to find the biggest element
+	 * @param leftIndex Inferior limit
+	 * @param rightIndex Upper limit
+	 * @return The biggest element of the vector
+	 */
+	private int biggestElement(Integer[] array, int leftIndex, int rightIndex) {
+		int biggest = array[leftIndex];
+		
+		for (int i = leftIndex; i < rightIndex + 1; i++) {
+			if (array[i] >= biggest) {
+				biggest = array[i];
+			}
+		}
+		return biggest;
+		
+	}
+	 
+	/**
+	 * It creates a copy of the array given in the first parameter of the method
+	 * 
+	 * @param array Array to be copied
+	 * @param secondArray Auxiliary array 
+	 */
+	private void copy(Integer[] array, Integer[] secondArray) {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = secondArray[i];
+		}
+	}
 }
--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -12,8 +12,81 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		
+		if (leftIndex > rightIndex || array == null || array.length == 0 || rightIndex > array.length || leftIndex > array.length) {
+			return;
+		}
+		
+		int k = biggestElement(array, leftIndex, rightIndex);
+		int m = smallestElement(array, leftIndex, rightIndex);
+		
+		Integer[] b = new Integer[k -m + 1];
+		fillArray(b);
+		
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			b[array[i] - m]++;
+		}
+		
+		int i = leftIndex;
+		int j = leftIndex;
+		
+		while (i < b.length) {
+			while (b[i] != 0) {
+				array[j] = i + m;
+				j++;
+				b[i]--;
+			}
+			i++;
+		}
+	}
+
+	/**
+	 * It fills in all spaces of the vector with 0
+	 * 
+	 * @param array Vector to be filled
+	 */
+	private void fillArray(Integer[] array) {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = 0;
+		}
+	}
+
+	/**
+	 * It returns the smallest element of the vector
+	 * 
+	 * @param array Array to find the smallest element
+	 * @param leftIndex Inferior limit
+	 * @param rightIndex Upper limit
+	 * @return The smallest element of the vector
+	 */
+	private int smallestElement(Integer[] array, int leftIndex, int rightIndex) {
+		int smallest = leftIndex;;
+		
+		for (int i = leftIndex; i < rightIndex + 1; i++) {
+			if (array[i] < smallest) {
+				smallest = array[i];
+			}
+		}
+		return smallest;
+	}
+
+	/**
+	 * It returns the biggest element of the vector
+	 * 
+	 * @param array Array to find the biggest element
+	 * @param leftIndex Inferior limit
+	 * @param rightIndex Upper limit
+	 * @return The biggest element of the vector
+	 */
+	private int biggestElement(Integer[] array, int leftIndex, int rightIndex) {
+		int biggest = leftIndex;
+		
+		for (int i = leftIndex; i < rightIndex + 1; i++) {
+			if (array[i] >= biggest) {
+				biggest = array[i];
+			}
+		}
+		return biggest;
 	}
 
 }
