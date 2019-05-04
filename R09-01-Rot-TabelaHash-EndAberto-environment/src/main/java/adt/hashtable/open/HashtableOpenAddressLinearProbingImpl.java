dba9cbee8307437b943a50ebeb917a7f7e044f96
Gabriel Fernandes <gabrielwebr@gmail.com>
2019-05-04 01:36:29 -0300

    Finish R09

--- R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
+++ R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
@@ -15,26 +15,81 @@ public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
 
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (isFull()) 
+			throw new HashtableOverflowException();
+		
+		if (element!= null) {
+			for (int i = 0; i < table.length; i++) {
+				int key = ((HashFunctionLinearProbing<T>) getHashFunction()).hash(element, i);
+				
+				if (element instanceof Storable) {
+					T auxKey = (T) table[key];				
+					if (auxKey == null || auxKey.equals(deletedElement)){
+						super.table[key] = element;
+						super.elements += 1;
+						break;
+					}
+					COLLISIONS ++;
+				}
+			}
+		}
 	}
 
 	@Override
 	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (!isEmpty() && element!=null) {
+			for (int i = 0; i < table.length; i++) {
+				int key = ((HashFunctionLinearProbing<T>) getHashFunction()).hash(element,i);
+				
+				if (table[key] instanceof Storable) {
+					T auxKey = (T) table[key];
+					
+					if (auxKey != null) {
+						if (auxKey.equals(element)) {
+							super.table[key] = deletedElement;
+							super.elements -= 1;
+						}
+						COLLISIONS --;
+					}
+				}
+			}
+		}
 	}
 
 	@Override
 	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (!isEmpty() && element!=null) {
+			for (int i = 0; i < table.length; i++) {
+				int key = ((HashFunctionLinearProbing<T>) getHashFunction()).hash(element, i);
+				
+				if (table[key] instanceof Storable) {
+					T auxKey = (T) super.table[key];	
+					if (auxKey != null) {
+						if (auxKey.equals(element))
+							return auxKey;
+					}
+				}
+			}
+		}
+		return null;
 	}
 
 	@Override
 	public int indexOf(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (!isEmpty() && element!=null) { 
+			for (int i = 0; i < super.table.length; i++) {
+				int key = ((HashFunctionLinearProbing<T>) getHashFunction()).hash(element, i);
+				
+				if (super.table[key] instanceof Storable ) {
+					T auxKey = (T) super.table[key];
+					
+					if (auxKey != null ) 
+						if (!auxKey.equals(deletedElement ) && auxKey.equals(element)) 
+							return key;
+				}
+			}
+		}
+		return -1;
 	}
 
 }
--- R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
+++ R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
@@ -15,25 +15,83 @@ public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable>
 
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (isFull())
+			throw new HashtableOverflowException();
+		
+		if (element != null) {
+	    	for (int i = 0; i < table.length; i++) {
+	    		int key = ((HashFunctionQuadraticProbing<T>) getHashFunction()).hash(element, i);
+	    		if (element instanceof Storable ) {
+	   				T auxKey = (T) table[key];
+	   				
+	    			if (auxKey == null || auxKey.equals(deletedElement)) {
+	    				super.table[key] = element;
+	    				super.elements ++;	                
+	   					break;
+	   				}
+	   				super.COLLISIONS ++;
+	   			}
+	   		}
+		}
 	}
 
 	@Override
 	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (!isEmpty() && element != null) {
+			for (int i = 0; i < table.length; i++ ) {
+				int key = ((HashFunctionQuadraticProbing<T>) getHashFunction()).hash(element, i);
+				
+				if (table[key] instanceof Storable) {
+					T auxKey = (T) table[key];
+					
+					if (auxKey != null) {
+						if (auxKey.equals(element)) {
+							super.table[key] = deletedElement;
+							super.elements --;
+						}
+					}
+				}
+			}
+		}
 	}
 
 	@Override
 	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (!isEmpty() && element != null) {
+			for (int i = 0; i < table.length; i++) {
+				int key = ((HashFunctionQuadraticProbing<T>) getHashFunction()).hash(element, i);
+				
+				if (table[key] instanceof Storable) {
+					T auxKey = (T) super.table[key];
+					
+					if (auxKey != null) {
+						if (auxKey.equals(element)) {
+							return auxKey;
+						}
+					}
+				}
+			}
+		}
+		return null;
 	}
 
 	@Override
 	public int indexOf(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (!isEmpty() && element != null) {
+			for (int i = 0; i < table.length; i++) {
+				int key = ((HashFunctionQuadraticProbing<T>) getHashFunction()).hash(element, i);
+				
+				if (super.table[key] instanceof Storable) {
+					T auxKey = (T) super.table[key];
+					
+					if (auxKey != null) {
+						if (!auxKey.equals(deletedElement) && auxKey.equals(element)) {
+							return key;
+						}
+					}
+				}
+			}
+		}
+		return -1;
 	}
 }
